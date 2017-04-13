523b0d3cd5f2ab7af4e8abafa5a58fe2288c12cd
Simon Eskildsen <sirup@sirupsen.com>
2017-04-13 16:32:23 -0400

    wip

--- lib/redis/semaphore.rb
+++ lib/redis/semaphore.rb
@@ -4,6 +4,9 @@ class Redis
   class Semaphore
     EXISTS_TOKEN = "1"
     API_VERSION = "1"
+    DEFAULT_CREATE_RELEASE_INTERVAL = 30
+
+    attr_accessor :redis, :create_release_interval
 
     # stale_client_timeout is the threshold of time before we assume
     # that something has gone terribly wrong with a client and we
@@ -19,26 +22,15 @@ class Redis
       @expiration = opts.delete(:expiration)
       @resource_count = opts.delete(:resources) || 1
       @stale_client_timeout = opts.delete(:stale_client_timeout)
-      @redis = opts.delete(:redis) || Redis.new(opts)
-      @use_local_time = opts.delete(:use_local_time)
+      @create_release_interval = opts.delete(:create_release_interval) || DEFAULT_CREATE_RELEASE_INTERVAL
+      @redis = opts.delete(:redis)
       @tokens = []
     end
 
     def exists_or_create!
       token = @redis.getset(exists_key, EXISTS_TOKEN)
-
-      if token.nil?
-        create!
-      else
-        # Previous versions of redis-semaphore did not set `version_key`.
-        # Make sure it's set now, so we can use it in future versions.
-
-        if token == API_VERSION && @redis.get(version_key).nil?
-          @redis.set(version_key, API_VERSION)
-        end
-
-        true
-      end
+      return true if token
+      create!
     end
 
     def available_count
@@ -56,10 +48,7 @@ class Redis
       @redis.del(version_key)
     end
 
-    def lock(timeout = nil)
-      exists_or_create!
-      release_stale_locks! if check_staleness?
-
+    def lock(timeout = 0, should_retry = true, &block)
       if timeout.nil? || timeout > 0
         # passing timeout 0 to blpop causes it to block
         _key, current_token = @redis.blpop(available_key, timeout || 0)
@@ -67,7 +56,15 @@ class Redis
         current_token = @redis.lpop(available_key)
       end
 
-      return false if current_token.nil?
+      unless current_token
+        every('create_and_release', @create_release_interval) do
+          exists_or_create!
+          release_stale_locks! if check_staleness?
+          return lock(timeout, false, &block) if should_retry
+        end
+
+        return false
+      end
 
       @tokens.push(current_token)
       @redis.hset(grabbed_key, current_token, current_time.to_f)
@@ -102,9 +99,7 @@ class Redis
       end
     end
 
-    def signal(token = 1)
-      token ||= generate_unique_token
-
+    def signal(token)
       @redis.multi do
         @redis.hdel grabbed_key, token
         @redis.lpush available_key, token
@@ -117,24 +112,8 @@ class Redis
       @redis.exists(exists_key)
     end
 
-    def all_tokens
-      @redis.multi do
-        @redis.lrange(available_key, 0, -1)
-        @redis.hkeys(grabbed_key)
-      end.flatten
-    end
-
-    def generate_unique_token
-      tokens = all_tokens
-      token = Random.rand.to_s
-
-      while(tokens.include? token)
-        token = Random.rand.to_s
-      end
-    end
-
     def release_stale_locks!
-      simple_expiring_mutex(:release_locks, 10) do
+      simple_expiring_mutex(:release_locks, @stale_client_timeout) do
         @redis.hgetall(grabbed_key).each do |token, locked_at|
           timed_out_at = locked_at.to_f + @stale_client_timeout
 
@@ -209,16 +188,8 @@ class Redis
       !@stale_client_timeout.nil?
     end
 
-    def redis_namespace?
-      (defined?(Redis::Namespace) && @redis.is_a?(Redis::Namespace))
-    end
-
     def namespaced_key(variable)
-      if redis_namespace?
-        "#{@name}:#{variable}"
-      else
-        "SEMAPHORE:#{@name}:#{variable}"
-      end
+      "SEMAPHORE:#{@name}:#{variable}"
     end
 
     def available_key
@@ -237,18 +208,16 @@ class Redis
       @version_key ||= namespaced_key('VERSION')
     end
 
-    def current_time
-      if @use_local_time
-        Time.now
-      else
-        begin
-          instant = redis_namespace? ? @redis.redis.time : @redis.time
-          Time.at(instant[0], instant[1])
-        rescue
-          @use_local_time = true
-          current_time
-        end
+    def every(key, timeout)
+      @every ||= {}
+      if !@every[key] || current_time >= @every[key] + timeout
+        @every[key] = Time.now
+        yield
       end
+    end
+
+    def current_time
+      Time.now
     end
   end
 end
--- spec/semaphore_spec.rb
+++ spec/semaphore_spec.rb
@@ -37,26 +37,26 @@ describe "redis" do
     end
 
     it "should lock and unlock" do
-      semaphore.lock(1)
+      semaphore.lock
       expect(semaphore.locked?).to eq(true)
       semaphore.unlock
       expect(semaphore.locked?).to eq(false)
     end
 
     it "should not lock twice as a mutex" do
-      expect(semaphore.lock(1)).not_to eq(false)
-      expect(semaphore.lock(1)).to eq(false)
+      expect(semaphore.lock).not_to eq(false)
+      expect(semaphore.lock).to eq(false)
     end
 
     it "should not lock three times when only two available" do
-      expect(multisem.lock(1)).not_to eq(false)
-      expect(multisem.lock(1)).not_to eq(false)
-      expect(multisem.lock(1)).to eq(false)
+      expect(multisem.lock).not_to eq(false)
+      expect(multisem.lock).not_to eq(false)
+      expect(multisem.lock).to eq(false)
     end
 
     it "should always have the correct lock-status" do
-      multisem.lock(1)
-      multisem.lock(1)
+      multisem.lock
+      multisem.lock
 
       expect(multisem.locked?).to eq(true)
       multisem.unlock
@@ -68,7 +68,7 @@ describe "redis" do
     it "should get all different tokens when saturating" do
       ids = []
       2.times do
-        ids << multisem.lock(1)
+        ids << multisem.lock
       end
 
       expect(ids).to eq(%w(0 1))
@@ -76,7 +76,7 @@ describe "redis" do
 
     it "should execute the given code block" do
       code_executed = false
-      semaphore.lock(1) do
+      semaphore.lock do
         code_executed = true
       end
       expect(code_executed).to eq(true)
@@ -84,7 +84,7 @@ describe "redis" do
 
     it "should pass an exception right through" do
       expect {
-        semaphore.lock(1) do
+        semaphore.lock do
           raise Exception, "redis semaphore exception"
         end
       }.to raise_error(Exception, "redis semaphore exception")
@@ -92,7 +92,7 @@ describe "redis" do
 
     it "should not leave the semaphore locked after raising an exception" do
       expect {
-        semaphore.lock(1) do
+        semaphore.lock do
           raise Exception, "redis semaphore exception"
         end
       }.to raise_error(Exception, "redis semaphore exception")
@@ -101,17 +101,17 @@ describe "redis" do
     end
 
     it "should return the value of the block if block-style locking is used" do
-      block_value = semaphore.lock(1) do
+      block_value = semaphore.lock do
         42
       end
       expect(block_value).to eq(42)
     end
 
     it "can return the passed in token to replicate old behaviour" do
-      lock_token = semaphore.lock(1)
+      lock_token = semaphore.lock
       semaphore.unlock()
 
-      block_value = semaphore.lock(1) do |token|
+      block_value = semaphore.lock do |token|
         token
       end
       expect(block_value).to eq(lock_token)
@@ -168,44 +168,6 @@ describe "redis" do
     end
   end
 
-  describe "semaphore without staleness checking" do
-    let(:semaphore) { Redis::Semaphore.new(:my_semaphore, :redis => @redis) }
-    let(:multisem) { Redis::Semaphore.new(:my_semaphore_2, :resources => 2, :redis => @redis) }
-
-    it_behaves_like "a semaphore"
-
-    it "can dynamically add resources" do
-      semaphore.exists_or_create!
-
-      3.times do
-        semaphore.signal
-      end
-
-      expect(semaphore.available_count).to eq(4)
-
-      semaphore.wait(1)
-      semaphore.wait(1)
-      semaphore.wait(1)
-
-      expect(semaphore.available_count).to eq(1)
-    end
-
-    it "can have stale locks released by a third process" do
-      watchdog = Redis::Semaphore.new(:my_semaphore, :redis => @redis, :stale_client_timeout => 1)
-      semaphore.lock
-
-      sleep 0.5
-
-      watchdog.release_stale_locks!
-      expect(semaphore.locked?).to eq(true)
-
-      sleep 0.6
-
-      watchdog.release_stale_locks!
-      expect(semaphore.locked?).to eq(false)
-    end
-  end
-
   describe "semaphore with staleness checking" do
     let(:semaphore) { Redis::Semaphore.new(:my_semaphore, :redis => @redis, :stale_client_timeout => 5) }
     let(:multisem) { Redis::Semaphore.new(:my_semaphore_2, :resources => 2, :redis => @redis, :stale_client_timeout => 5) }
@@ -213,64 +175,80 @@ describe "redis" do
     it_behaves_like "a semaphore"
 
     it "should restore resources of stale clients" do
-      hyper_aggressive_sem = Redis::Semaphore.new(:hyper_aggressive_sem, :resources => 1, :redis => @redis, :stale_client_timeout => 1)
+      hyper_aggressive_sem = Redis::Semaphore.new(:hyper_aggressive_sem, :resources => 1, :redis => @redis, :stale_client_timeout => 2)
 
-      expect(hyper_aggressive_sem.lock(1)).not_to eq(false)
-      expect(hyper_aggressive_sem.lock(1)).to eq(false)
-      expect(hyper_aggressive_sem.lock(1)).not_to eq(false)
-    end
-  end
-
-  describe "redis time" do
-    let(:semaphore) { Redis::Semaphore.new(:my_semaphore, :redis => @redis, :stale_client_timeout => 5) }
-
-    before(:all) do
-      Timecop.freeze(Time.local(1990))
-    end
-
-    it "with time support should return a different time than frozen time" do
-      expect(semaphore.send(:current_time)).not_to eq(Time.now)
-    end
-
-    it "with use_local_time should return the same time as frozen time" do
-      semaphore = Redis::Semaphore.new(:my_semaphore, :redis => @redis, :stale_client_timeout => 5, :use_local_time => true)
-      expect(semaphore.send(:current_time)).to eq(Time.now)
-    end
-
-    it "without time support should return the same time as frozen time" do
-      expect(@redis).to receive(:time).and_raise(Redis::CommandError)
-      expect(semaphore.send(:current_time)).to eq(Time.now)
-    end
-  end
-
-  describe "all_tokens" do
-    let(:semaphore) { Redis::Semaphore.new(:my_semaphore, :redis => @redis, :stale_client_timeout => 5) }
-
-    it "includes tokens from available and grabbed keys" do
-      semaphore.exists_or_create!
-      available_keys = semaphore.all_tokens
-      semaphore.lock(1)
-      grabbed_keys = semaphore.all_tokens
-
-      expect(available_keys).to eq(grabbed_keys)
-    end
-  end
-
-  describe "version" do
-    context "with an existing versionless semaphore" do
-      let(:old_sem) { Redis::Semaphore.new(:my_semaphore, :redis => @redis) }
-      let(:semaphore) { Redis::Semaphore.new(:my_semaphore, :redis => @redis) }
-      let(:version_key) { old_sem.send(:version_key) }
-
-      before do
-        old_sem.exists_or_create!
-        @redis.del(version_key)
+      expect(hyper_aggressive_sem.lock).not_to eq(false)
+      expect(hyper_aggressive_sem.lock).to eq(false)
+      Timecop.travel(Time.now + hyper_aggressive_sem.create_release_interval) do
+        expect(hyper_aggressive_sem.lock).not_to eq(false)
       end
+    end
+  end
 
-      it "sets the version key" do
-        semaphore.exists_or_create!
-        expect(@redis.get(version_key)).not_to be_nil
-      end
+  describe "minimize redis queries" do
+    it "queries while creating" do
+      mock = double()
+      expect(mock).to receive("lpop") { nil }
+
+      # create lock
+      expect(mock).to receive("getset") { nil }
+      expect(mock).to receive("expire") { nil }
+      expect(mock).to receive("multi") { true }.exactly(2).times
+
+      expect(mock).to receive("lpop") { "1" }
+      expect(mock).to receive("hset")
+
+      semaphore = Redis::Semaphore.new(:test, redis: mock)
+      semaphore.lock {}
+    end
+
+    it "queries when acquiring semaphore" do
+      mock = double()
+      expect(mock).to receive("lpop") { "1" }
+      expect(mock).to receive("hset")
+      expect(mock).to receive("multi")
+
+      semaphore = Redis::Semaphore.new(:test, redis: mock)
+      semaphore.lock {}
+    end
+
+    it "queries when releasing stale locks" do
+      mock = double()
+      expect(mock).to receive("lpop") { nil }
+
+      # create
+      expect(mock).to receive("getset") { "1" }
+
+      # lock for releasing
+      expect(mock).to receive("setnx") { true }
+      expect(mock).to receive("del") { true }
+
+      # release stale locks
+      expect(mock).to receive("hgetall") { [["0", Time.now - 11]] }
+
+      # retry and get lock
+      expect(mock).to receive("lpop") { "0" }
+      expect(mock).to receive("hset")
+
+      # release stale lock
+      # release own lock
+      expect(mock).to receive("multi").exactly(2).times
+
+      semaphore = Redis::Semaphore.new(:test, redis: mock, stale_client_timeout: 10)
+      semaphore.lock {}
+    end
+
+    it "queries less when it has recently released stale locks" do
+      semaphore = Redis::Semaphore.new(:test, redis: @redis, stale_client_timeout: 10)
+      semaphore.release_stale_locks!
+
+      mock = double()
+      expect(mock).to receive("lpop") { "1" }
+      expect(mock).to receive("hset")
+      expect(mock).to receive("multi")
+
+      semaphore.redis = mock
+      semaphore.lock {}
     end
   end
 
